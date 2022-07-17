c9ac70ad6f69e8d4bfde77d29c015f489090d66f
Ivanchov <lubcho224@gmail.com>
2022-07-17 17:48:11 +0300

    prerelise

--- sc.js
+++ sc.js
@@ -1,12 +1,11 @@
 const dp = document.getElementById('display');
-var numbers = ['num_0', 'num_1'];
+var numbers = ['num_0', 'num_1', "num_2", "num_3", "num_4", "num_5", "num_6", "num_7", "num_8", "num_9", "num_del"];
 for(var i = 0; i < numbers.length; i++){
-    switch (document.getElementById(numbers[i])){
-        case 'num_0':
-            alert("hello");
-    }
-}
-var num = document.getElementById(numbers[1]);
-num.onclick = () =>{
-    alert("hello");
+    document.getElementById(numbers[i]).addEventListener("click", (e)=> {
+       e.target = dp.append(e.target.value);
+       
+
+
+    })
+   
 }
